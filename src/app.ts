fdad2fc05df536fd5699bfad799720d5f15c26db
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-07-29 14:46:53 -0300

    Tratando erros e configurando app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
+import "express-async-errors";
 import * as express from "express";
+import * as cors from "cors";
+import * as logger from "morgan";
+
+import { connectionServerDB } from "./config/db";
+import { errorMiddleware } from "./app/middlewares/error";
 
 export const app = express();
 
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cors());
+
+app.use(errorMiddleware);
+
+connectionServerDB();
