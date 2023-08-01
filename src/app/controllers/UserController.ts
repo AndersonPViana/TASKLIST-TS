95a63fa9a2f53d41e8a64344ca5e10681a1ea54d
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-08-01 09:43:02 -0300

    Update password & routes

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import * as bcrypt from "bcrypt";
 
 import { userRepository } from "../repositories/userRepository";
-import { badRequestError } from "../helpers/apiError";
+import { badRequestError, unauthorizedError } from "../helpers/apiError";
+import CustomRequest from "../interface/CustomInterface";
 
 class UserController {
   async create(req: Request, res: Response): Promise<Response> {
@@ -25,6 +26,30 @@ class UserController {
       name, 
       email
     });
+  }
+
+  async update(req: CustomRequest, res: Response): Promise<Response> {
+    const { email, oldPassword, newPassword } = req.body;
+
+    const user = req.user;
+
+    if(email !== user.email) {
+      const userExist = await userRepository.findOneBy({ email });
+
+      if(userExist) {
+        throw new badRequestError("user already exists");
+      }
+    }
+
+    if(oldPassword && !(await bcrypt.compare(oldPassword, user.password_hash))) {
+      throw new unauthorizedError("incorrect password");
+    }
+
+    const password_hash = await bcrypt.hash(newPassword, 8);
+
+    await userRepository.update(user.id, { password_hash });
+
+    return res.json({ message: "updated password" });
   }
 }
 
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 
 import UserController from "../controllers/UserController";
+import SessionController from "../controllers/SessionController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 export const routes = Router();
 
 routes.post("/users", UserController.create);
+
+routes.post("/sessions", SessionController.create);
+
+routes.use(authMiddleware);
+
+routes.put("/users", UserController.update);
